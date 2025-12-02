335c465d8fd675837452cbcba08cfd39ac09dfc9
jotiram83 <jotiram@gmail.com>
2025-12-03 00:49:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-import connector as cnx
+import snowflake.connector as cnx
 from snowflake.snowpark.functions import col
 
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
